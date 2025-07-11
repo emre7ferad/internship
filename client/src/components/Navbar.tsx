0c4af0b41883753385a5177ad610e70f84b1b567
Emre Ferad <emre_ferad711@abv.bg>
2025-07-11 15:25:49 +0300

    Update Navbar.tsx: [Logo is clickable]

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -13,7 +13,9 @@ const Navbar = ({ loggedIn } : NavbarProps) => {
     return (
         <header className='bg-white shadow-md px-6 py-4 flex justify-between items-center'>
             <div className='flex items-center space-x-4'>
-                <img src="/logo.png" alt="Fibank Logo" className='h-10'/>
+                <Link to=''>
+                    <img src="/logo.png" alt="Fibank Logo" className='h-10'/>
+                </Link>
             </div>
 
                 {loggedIn ? (
@@ -23,31 +25,31 @@ const Navbar = ({ loggedIn } : NavbarProps) => {
                             <div></div>
                             <div className='flex items-center space-x-6'>
                                 <button>ENGLISH</button>
-                                <div className='relative flex items-center space-x-1 cursor-pointer hover:text-blue-600'>
+                                <a href='#' className='relative flex items-center space-x-1 cursor-pointer hover:text-blue-800'>
                                     <div className='relative'>
                                         <FaEnvelope className='text-lg'/>
                                         <span className='absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full px-1'>2</span>
                                     </div>
                                     <span>СЪОБЩЕНИЯ</span>
-                                </div>
-                                <div className='relative flex items-center space-x-1 cursor-pointer hover:text-blue-600'>
+                                </a>
+                                <a href='#' className='relative flex items-center space-x-1 cursor-pointer hover:text-blue-800'>
                                     <div className='relative'>
                                         <FaBell className='text-lg'/>
                                         <span className='absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full px-1'>3</span>
                                     </div>
                                     <span>ИЗВЕСТИЯ</span>
-                                </div>
-                                <div className='relative flex items-center space-x-1 cursor-pointer hover:text-blue-600'>
+                                </a>
+                                <a href='#' className='relative flex items-center space-x-1 cursor-pointer hover:text-blue-800'>
                                     <FaCog/>
-                                    <span className='relative flex items-center space-x-1 cursor-pointer hover:text-blue-600'>НАСТРОЙКИ</span>
-                                </div>
+                                    <span className='relative flex items-center space-x-1 cursor-pointer hover:text-blue-800'>НАСТРОЙКИ</span>
+                                </a>
                                 <div>
-                                    <img src="/profile.jpg" alt="Profile" className='h-8 w-8 rounded-full border border-gray-300 hover:cursor-pointer' />
+                                    <a href='#'><img src="/profile.jpg" alt="Profile" className='h-8 w-8 rounded-full border border-gray-300 hover:cursor-pointer' /></a>
                                 </div>
-                                <div className='flex items-center space-x-1 cursor-pointer hover:text-red-600'>
+                                <a href='#' className='flex items-center space-x-1 cursor-pointer hover:text-red-600'>
                                     <FaPowerOff/>
                                     <span>ИЗХОД</span>
-                                </div>
+                                </a>
                             </div>
                         </div>
                     </>
@@ -55,20 +57,20 @@ const Navbar = ({ loggedIn } : NavbarProps) => {
                     <>
                         <div className='flex-grow flex justify-center space-x-6 text-sm text-gray-500'>
                             <button>ENGLISH</button>
-                            <a href="#" className='flex items-center space-x-1 hover:text-blue-600'>
+                            <a href="#" className='flex items-center space-x-1 hover:text-blue-800'>
                                 <FaDesktop/>
                                 Към Сайта
                             </a>
-                            <a href="#" className='flex items-center space-x-1 hover:text-blue-600'>
+                            <a href="#" className='flex items-center space-x-1 hover:text-blue-800'>
                                 <FaAndroid/>
                                 <FaApple/>
                                 <span>Мобилно Приложение</span>
                             </a>
-                            <a href="#" className='flex items-center space-x-1 hover:text-blue-600'>
+                            <a href="#" className='flex items-center space-x-1 hover:text-blue-800'>
                                 <FaClipboardList/>
                                 <span>Промени в ОУ трафика</span>
                             </a>
-                            <a href="#" className='flex items-center space-x-1 hover:text-blue-600'>
+                            <a href="#" className='flex items-center space-x-1 hover:text-blue-800'>
                                 <BsInfoLg/>
                                 <span>Помощ</span>
                             </a>
@@ -77,7 +79,7 @@ const Navbar = ({ loggedIn } : NavbarProps) => {
                         <div className='flex-shrink-0 space-x-2'>
                             {!isLoginPage && (
                                 <Link to="/login" 
-                                className='bg-blue-600 text-white font-bold px-8 py-2 hover:bg-blue-800 transition'>
+                                className='bg-blue-800 text-white font-bold px-8 py-2 hover:bg-blue-600 transition'>
                                     ВХОД
                                 </Link>
                             )}
